fd5b1198b2de827882c337a7ea784d75d08c4f68
abdel-az <drisselbaghdadibbt@gmail.com>
2022-08-06 01:12:16 +0100

    Tutorial 4

--- angular/angular-app/src/app/app.module.ts
+++ angular/angular-app/src/app/app.module.ts
@@ -1,15 +1,12 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TaskListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
