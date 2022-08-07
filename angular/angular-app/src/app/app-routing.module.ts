47f651bb8c087c472dee39ed9e35e781fed1cb96
abdel-az <drisselbaghdadibbt@gmail.com>
2022-08-07 14:53:07 +0100

    Tutorial 6

--- angular/angular-app/src/app/app-routing.module.ts
+++ angular/angular-app/src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
+
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './auth/auth.guard';
 
+import { TaskListComponent } from './task-list/task-list.component';
+import { CallbackComponent } from './callback/callback.component';
+
+const routes: Routes = [
+  {
+    path: 'tasks',
+    component: TaskListComponent,
+    canActivate: [ AuthGuard ]
+  },
+  {
+    path: 'callback', component: CallbackComponent
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
